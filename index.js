d6530a30a106660ca7217be6bc434bb50ffdeabb
skytool <albert.do@aol.com>
2023-05-13 23:34:33 -0500

    I need more to do it

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email){
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json",
+        },
+        
+        body : JSON.stringify({
+            name: "Ooggy",
+            email: "adb@example.com",
+        }),
+    })
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (obj) {
+            console.log(obj);
+        })
+        .catch(function (error){
+            alert("it is an error!!");
+            console.log(error.message);
+        })
+
+    }
